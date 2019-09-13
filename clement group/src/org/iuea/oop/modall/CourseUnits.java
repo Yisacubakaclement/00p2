75ea93e030fe9893f3da2efce8488212ee1725de
Yisacubakaclement <47665832+Yisacubakaclement@users.noreply.github.com>
2019-09-13 08:42:19 -0500

    Add files via upload

---
+++ clement group/src/org/iuea/oop/modall/CourseUnits.java
@@ -0,0 +1,21 @@
+/*Group members 
+ Kaniki Asanga Christine 17/272/BIT-j
+
+*/
+package org.iuea.oop.modall;
+
+//creating the courseUnits class that holds details of a courseUnit
+public class CourseUnits {
+    //creating the attributes of the class
+    static String cUnitname;
+    static String creditUnit;
+    static String cUnitdescription;
+    static String cUnitcontent;
+    static String coursecode;
+    static String cUnityear;
+    
+    //creating the main method to make the program be able to run
+    public static void main(String[]args){
+        
+    }
+}
